4eae368fa2e84023a8c095e32bd8762e6ffc415f
Thanatermesis <thanatermesis@gmail.com>
2014-04-08 11:41:14 +0200

    changed some aliases

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -21,7 +21,7 @@ setopt CORRECT
 alias ack='nocorrect ack'
 alias cd='nocorrect cd'
 alias cp='nocorrect cp'
-alias ebuild='nocorrect ebuild'
+#alias ebuild='nocorrect ebuild'
 alias gcc='nocorrect gcc'
 alias gist='nocorrect gist'
 alias grep='nocorrect grep'
@@ -45,16 +45,16 @@ alias rm='nocorrect rm'
 #alias sftp='noglob sftp'
 
 # Define general aliases.
-alias _='sudo'
-alias b='${(z)BROWSER}'
+#alias _='sudo'
+#alias b='${(z)BROWSER}'
 #alias cp="${aliases[cp]:-cp} -i"
-alias e='${(z)VISUAL:-${(z)EDITOR}}'
+#alias e='${(z)VISUAL:-${(z)EDITOR}}'
 alias ln="${aliases[ln]:-ln} -i"
 alias mkdir="${aliases[mkdir]:-mkdir} -p"
 alias mv="${aliases[mv]:-mv} -i"
-alias p='${(z)PAGER}'
-alias po='popd'
-alias pu='pushd'
+#alias p='${(z)PAGER}'
+#alias po='popd'
+#alias pu='pushd'
 #alias rm="${aliases[rm]:-rm} -i"
 alias type='type -a'
 
@@ -131,8 +131,8 @@ elif (( $+commands[wget] )); then
 fi
 
 # Resource Usage
-alias df='df -kh'
-alias du='du -kh'
+alias df='df -k'
+alias du='du -k'
 
 if (( $+commands[htop] )); then
   alias top=htop
@@ -154,6 +154,7 @@ alias http-serve='python -m SimpleHTTPServer'
 function mkdcd {
   [[ -n "$1" ]] && mkdir -p "$1" && builtin cd "$1"
 }
+alias take="mkdcd"
 
 # Changes to a directory and lists its contents.
 function cdls {
